59914f495ad002b9d7b7062fb8031b9c81567152
Murat GOKDUMAN <drmuratgokduman@gmail.com>
2022-12-27 00:46:25 +0300

    Clean Blog Projesi|Odev3

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const path = require('path');
 const ejs = require('ejs');
 
+const Post = require('./modals/Post');
+
 const app = express();
 
-app.set("view engine", "ejs");
+// DB bağlantı:
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost/cleanBlog-test-db');
+
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', async (req, res) => {
+  const posts = await Post.find({})
+  res.render('index', {
+    posts: posts
+  });
 });
 app.get('/add_post', (req, res) => {
-  res.render('add_post')
+  res.render('add_post');
 });
 app.get('/about', (req, res) => {
-  res.render('about')
+  res.render('about');
 });
 app.get('/post', (req, res) => {
-  res.render('post')
+  res.render('post');
+});
+
+app.post('/message', async (req, res,) => {
+  await Post.create(req.body);
+  res.redirect('/add_post')
 });
 
 const port = 3000;
